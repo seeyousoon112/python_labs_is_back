d81cbc45d1a717a451832ba5c2a4bc1c3370f39d
seeyousoon112 <ksaveralexfold28@mail.ru>
2026-05-25 22:38:41 +0300

    fix 2.0

--- src/lab07/exceptions.py
+++ src/lab07/exceptions.py
@@ -1,13 +1,10 @@
-"""Собственные исключения для предметной области."""
+
 
 class DuplicateItemError(Exception):
-    """Объект с таким именем уже существует в коллекции."""
     pass
 
 class ItemNotFoundException(Exception):
-    """Объект не найден в коллекции."""
     pass
 
 class ValidationError(Exception):
-    """Ошибка валидации пользовательского ввода."""
     pass
